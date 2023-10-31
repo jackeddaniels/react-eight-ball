321fec1b67f7b597d77e1e26f3368486732d92fc
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-31 12:35:38 -0700

    Functioning EightBall.

--- src/EightBall.js
+++ src/EightBall.js
@@ -2,10 +2,32 @@ import answerList from "./answers";
 import { useState } from "react";
 import _ from "lodash";
 
+/** EightBall displaying random fortunes.
+ *
+ * Props:
+ * - answers: Array of objects containing keys msg and color
+ *
+ * State:
+ * - answer: Objects containing keys msg and color
+ *
+ * App -> EightBall
+ */
+
 function EightBall({answers = answerList}) {
   const [answer, setAnswer] = useState({ color: 'black', msg: 'Think of a Question' });
 
-  function updateEightball() {
-    setAnswer(_.choice(answers));
+  function updateEightBall() {
+    setAnswer(_.sample(answers));
   }
+
+  return (
+    <div
+      id="eight-ball"
+      style={ {backgroundColor: answer.color} }
+      onClick={updateEightBall}>
+      <p id="text">{ answer.msg }</p>
+    </div>
+  )
 }
+
+export default EightBall;
